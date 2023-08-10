cee6fc674cb197e9f2fd73557e3aaa81375e6cf1
maxecharles <max.charles@sydney.edu.au>
2023-08-10 14:09:38 +1000

    reformat

--- bases.py
+++ bases.py
@@ -20,11 +20,19 @@ def jwst_hexike_bases(nterms=10, npix=1024, AMI=False):
         # shifts from WebbPSF: CV3 on-orbit estimate (RPT028027) + OTIS delta from predicted (037134)
         shifts = const.JWST_CIRCUMSCRIBED_DIAMETER * np.array([0.0243, -0.0141])
         amplitude_plane = 3
-        keys = ['B2-9', 'B3-11', 'B4-13', 'C5-16', 'B6-17', 'C1-8', 'C6-18']  # the segments that are used in AMI
-        niriss.pupil_mask = 'MASK_NRM'
+        keys = [
+            "B2-9",
+            "B3-11",
+            "B4-13",
+            "C5-16",
+            "B6-17",
+            "C1-8",
+            "C6-18",
+        ]  # the segments that are used in AMI
+        niriss.pupil_mask = "MASK_NRM"
 
     else:
-        seg_rad = const.JWST_SEGMENT_RADIUS  # 1.32 * 1.001  # TODO check for overlapping pixels
+        seg_rad = const.JWST_SEGMENT_RADIUS  # TODO check for overlapping pixels
         shifts = np.zeros(2)  # no shift for full pupil
         amplitude_plane = 0
         keys = const.SEGNAMES_WSS  # all mirror segments
@@ -41,35 +49,54 @@ def jwst_hexike_bases(nterms=10, npix=1024, AMI=False):
     bases = []
     for key in keys:  # cycling through segments
         centre = np.array(seg_cens[key]) - shifts
-        rhos, thetas = numpy.array(dlu.pixel_coordinates((npix, npix), pscale, offsets=tuple(centre), polar=True))
-        bases.append(hexike_basis(nterms, npix, rhos/seg_rad, thetas, outside=0.))  # appending basis
+        rhos, thetas = numpy.array(
+            dlu.pixel_coordinates(
+                (npix, npix), pscale, offsets=tuple(centre), polar=True
+            )
+        )
+        bases.append(
+            hexike_basis(nterms, npix, rhos / seg_rad, thetas, outside=0.0)
+        )  # appending basis
 
     if AMI:
-        return np.flip(np.array(bases), axis=(2, 3)), mask, pscale  # need to apply flip for AMI as plane 1 is a flip
+        return (
+            np.flip(np.array(bases), axis=(2, 3)),
+            mask,
+            pscale,
+        )  # need to apply flip for AMI as plane 1 is a flip
 
     else:
         return np.array(bases), mask, pscale
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     config.update("jax_enable_x64", True)
-    plt.rcParams["image.origin"] = 'lower'
-
+    plt.rcParams["image.origin"] = "lower"
 
     def plot_bases(bases, mask, npix, pscale, save=False, edges=False):
-        sample_bases = bases.sum(0) # * mask
+        sample_bases = bases.sum(0) * mask
         fig, ax = plt.subplots(2, 5, figsize=(12.5, 5))
 
-        fig.subplots_adjust(left=0.01, right=0.99, bottom=0.01, top=0.99, wspace=0., hspace=0.)
+        fig.subplots_adjust(
+            left=0.01, right=0.99, bottom=0.01, top=0.99, wspace=0.0, hspace=0.0
+        )
         for i in range(2):
             for j in range(5):
-                bound = np.array([sample_bases[i * 5 + j].max(), -sample_bases[i * 5 + j].min()]).max()
-                ax[i, j].imshow(sample_bases[i * 5 + j],
-                                cmap='seismic',
-                                vmin=-bound,
-                                vmax=bound,
-                                extent=(pscale * -npix / 2, pscale * npix / 2, pscale * -npix / 2, pscale * npix / 2)
-                                )
+                bound = np.array(
+                    [sample_bases[i * 5 + j].max(), -sample_bases[i * 5 + j].min()]
+                ).max()
+                ax[i, j].imshow(
+                    sample_bases[i * 5 + j],
+                    cmap="seismic",
+                    vmin=-bound,
+                    vmax=bound,
+                    extent=(
+                        pscale * -npix / 2,
+                        pscale * npix / 2,
+                        pscale * -npix / 2,
+                        pscale * npix / 2,
+                    ),
+                )
 
                 ax[i, j].set(xticks=[], yticks=[])
 
@@ -77,10 +104,17 @@ if __name__ == '__main__':
                     corners = const.JWST_PRIMARY_SEGMENTS
                     for segment in corners:
                         corner = segment[1].T
-                        ax[i, j].plot(corner[0], corner[1], marker='', c='k', alpha=0.5, linestyle='--')
+                        ax[i, j].plot(
+                            corner[0],
+                            corner[1],
+                            marker="",
+                            c="k",
+                            alpha=0.5,
+                            linestyle="--",
+                        )
 
         if save:
-            plt.savefig('hexike_bases.pdf', dpi=1000)
+            plt.savefig("hexike_bases.pdf", dpi=1000)
         plt.show()
 
     npix = 1024
